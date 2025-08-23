d50ea38591197c2fa605f390e8056b02be6f607c
Rishabhraj15298 <rishabhraj829112@gmail.com>
2025-08-23 14:23:38 +0530

    enumerate in python

---
+++ python-fundamentals/enumerate.py
@@ -0,0 +1,11 @@
+# MARKING WRONG ANSWERS IN A QUIZ
+
+answers = ['A', 'C', 'B', 'D', 'A']
+student_answers = ['A', 'C', 'D', 'D', 'A']
+
+for index , (correct , student) in enumerate(zip(answers , student_answers) , start=1):
+    if(correct == student):
+        print(f"Question {index} : Correct")
+    else:
+        print(f"Question {index} : Wrong  (Your answer : {student} , Correct answer : {correct} )" )
+        
